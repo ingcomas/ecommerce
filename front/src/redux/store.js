4c945ca00b0384318606cb98628481f29870f80c
ingcomas <sebacomas>
2018-10-30 12:34:25 -0300

    third commit

--- front/src/redux/store.js
+++ front/src/redux/store.js
@@ -1,8 +1,8 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+// import { createStore, applyMiddleware, compose } from 'redux';
 // import { createLogger } from 'redux-logger';
-import thunkMiddleware from 'redux-thunk';
-import reducer from '../redux/reducers/movies-reducer';//cambiar la direccion donde se encuentra el combine reducer
+// import thunkMiddleware from 'redux-thunk';
+// import reducer from '../redux/reducers/movies-reducer';//cambiar la direccion donde se encuentra el combine reducer
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export default createStore(reducer, composeEnhancers(applyMiddleware(thunkMiddleware)));
+// export default createStore(reducer, composeEnhancers(applyMiddleware(thunkMiddleware)));
