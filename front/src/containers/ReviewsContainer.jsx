50a467ed15870c9dcc7fac2c84a497488e4a912f
santiagofz <santiago.fernandez1234@hotmail.com>
2018-11-05 18:54:14 -0300

    18:54

--- front/src/containers/ReviewsContainer.jsx
+++ front/src/containers/ReviewsContainer.jsx
@@ -10,7 +10,7 @@ export default class ReviewsContainer extends Component{
   render(){
     return (
       <div>
-      {/* <RatingWidget /> */}
+       <RatingWidget /> 
       <Reviews />
       </div>
     )
