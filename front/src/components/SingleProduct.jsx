3dafb39a5beb009d77f4b131e4afb579c9ba32e5
santiagofz <santiago.fernandez1234@hotmail.com>
2018-11-05 16:25:08 -0300

    view singleproduct and allproduct 5/11; 16:25

--- front/src/components/SingleProduct.jsx
+++ front/src/components/SingleProduct.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
 
-export default ({product})=> (
+export default ({product, addToCart})=> (
 <div className="row single-product">
      
       { product && <div className="col-sm-10 col-md-8 col-lg-8 cont-img-single-product">
       <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
   <div className="carousel-inner">
     <div className="carousel-item active">
-      <img className="d-block w-100" src="https://static.netshoes.com.ar/produtos/zapatillas-nike-tanjun/59/001-3204-059/001-3204-059_detalhe1.jpg?resize=326:*" alt="First slide"/>
+      <img className="d-block w-100 img-single-product" src="https://static.netshoes.com.ar/produtos/zapatillas-nike-tanjun/59/001-3204-059/001-3204-059_detalhe1.jpg?resize=326:*" alt="First slide"/>
     </div>
     <div className="carousel-item">
-      <img className="d-block w-100" src="https://dexter.vteximg.com.br/arquivos/ids/387059-540-540/921694011_1.jpg?v=636673437470100000" alt="Second slide"/>
+      <img className="d-block w-100 img-single-product" src="https://dexter.vteximg.com.br/arquivos/ids/387059-540-540/921694011_1.jpg?v=636673437470100000" alt="Second slide"/>
     </div>
     <div className="carousel-item">
-      <img className="d-block w-100" src="https://http2.mlstatic.com/zapatillas-de-cuero-hombre-fierros-art-09-talle-46-D_NQ_NP_737449-MLA25860951412_082017-F.jpg" alt="Third slide"/>
+      <img className="d-block w-100 img-single-product" src="https://http2.mlstatic.com/zapatillas-de-cuero-hombre-fierros-art-09-talle-46-D_NQ_NP_737449-MLA25860951412_082017-F.jpg" alt="Third slide"/>
     </div>
   </div>
   <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
@@ -35,6 +35,7 @@ export default ({product})=> (
       <p> STOCK: {product.stock}</p>
       {/* <p> {product}</p>
       <p> {product}</p> */}
+      <button onClick={()=>addToCart(product)}>add</button>
     </div> 
    } 
  
