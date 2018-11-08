c2a0e53281abda7d293eb221a28823e077cbc2a8
tyago343 <tyagocasanova@hotmail.com>
2018-11-08 11:07:27 -0300

    problemas

--- front/src/components/PrivateProfile.jsx
+++ front/src/components/PrivateProfile.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+ import React from 'react';
 import {Redirect} from 'react-router-dom'
 import AdminProfile from '../components/AdminProfile';
 import UserProfile from '../components/UserProfile';
@@ -12,16 +12,9 @@ render(){
     return(
         <div>
             {
-<<<<<<< HEAD
                 this.props.user.access == true ? 
                     <Redirect to='/user/admin' />
                     // <AdminProfile logOut={props.logOut}/>
-=======
-                props.user.access == true ? 
-                    <AdminProfile
-                        logOut={props.logOut}
-                    />
->>>>>>> ae9604d772d382cd7b99bf18147587dc3c4c0939
                 :
                     <UserProfile user={this.props.user} logOut={this.props.logOut}/> 
             }
